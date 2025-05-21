d7d1df4b01bd471970b5ba0801c22ce363703e84
WuX <wux@thenest.dev>
2025-05-21 15:50:50 +0300

    chore: update nix flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
     inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
         flake-utils.url = "github:numtide/flake-utils";
 
         chariot.url = "github:elysium-os/chariot";
@@ -8,25 +8,35 @@
     };
 
     outputs = { nixpkgs, flake-utils, ... } @ inputs: flake-utils.lib.eachDefaultSystem (system:
-        let pkgs = import nixpkgs { inherit system; }; in {
-            devShells.default = pkgs.mkShell {
-                shellHook = "export NIX_SHELL_NAME='elysium-os'";
-                nativeBuildInputs = with pkgs; [
-                    inputs.chariot.defaultPackage.${system}
-                    wget # Required by Chariot
-                    libarchive # Required by Chariot
+        let
+            pkgs = import nixpkgs { inherit system; };
+            commonPackages = with pkgs; [
+                inputs.chariot.defaultPackage.${system}
 
-                    python3 # Used heavily by tools
+                wget # Required by Chariot
+                libarchive # Required by Chariot
 
-                    gdb
-                    bochs
-                    qemu_full
-                    (pkgs.writeShellScriptBin "qemu-ovmf-x86-64" ''
-                        ${pkgs.qemu_full}/bin/qemu-system-x86_64 \
-                            -drive if=pflash,unit=0,format=raw,file=${pkgs.OVMF.fd}/FV/OVMF.fd,readonly=on \
-                            "$@"
-                    '')
-                ];
+                python3 # Used heavily by tools
+
+                gdb
+                bochs
+                qemu_full
+                (pkgs.writeShellScriptBin "qemu-ovmf-x86-64" ''
+                    ${pkgs.qemu_full}/bin/qemu-system-x86_64 \
+                        -drive if=pflash,unit=0,format=raw,file=${pkgs.OVMF.fd}/FV/OVMF.fd,readonly=on \
+                        "$@"
+                '')
+            ];
+        in {
+            devShells = {
+                default = pkgs.mkShell {
+                    shellHook = "export NIX_SHELL_NAME='elysium-os'";
+                    nativeBuildInputs = commonPackages;
+                };
+                zed = pkgs.mkShell {
+                    shellHook = "export NIX_SHELL_NAME='elysium-os/zed'";
+                    nativeBuildInputs = commonPackages ++ [ pkgs.zed-editor ];
+                };
             };
         }
     );
