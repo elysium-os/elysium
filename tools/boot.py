0205708d9d6678a8e8a7c143d70f8feebabe1265
WuX <wux@thenest.dev>
2025-05-22 16:47:51 +0300

    chore: clean up the boot script a bit

--- tools/boot.py
+++ tools/boot.py
@@ -17,7 +17,6 @@ parser.add_argument("--accel", default="kvm", choices=["kvm", "tcg"])
 parser.add_argument("--debug", action="store_true")
 parser.add_argument("-d", "--display", default="default", choices=["default", "headless", "vnc"])
 parser.add_argument("-n", "--cores", type=int, default=4)
-
 parser.add_argument("--gdb", action="store_true")
 parser.add_argument("--tcg", action="store_const", const="tcg", dest="accel")
 parser.add_argument("--headless", action="store_const", const="headless", dest="display")
@@ -27,17 +26,17 @@ args = parser.parse_args()
 chariot_opts = ["--option", f"buildtype={args.buildtype}"]
 
 # Build cronus & image
-for cmd in [
-    ["chariot", *chariot_opts, "build", "source/cronus"],
-    ["chariot", *chariot_opts, "--verbose", "build", "package/cronus"],
-    ["chariot", *chariot_opts, "--verbose", "build", "package/modules"],
-    ["chariot", *chariot_opts, "build", "custom/image"]
-]:
-    build_result = subprocess.run(cmd)
+chariot_cmd = ["chariot", "build"]
+chariot_cmd.extend([
+    "source/cronus",
+    "package/cronus",
+    "package/modules",
+    "custom/image",
+])
 
-    if build_result.returncode != 0:
-        print("Build failed, not running qemu")
-        exit(1)
+if subprocess.run(chariot_cmd).returncode != 0:
+    print("Build failed, not running qemu")
+    exit(1)
 
 # Run QEMU
 image_path = os.path.join(chariot_utils.chariot_path("custom/image", chariot_opts), "elysium_efi.img" if args.efi else "elysium_bios.img")
