16fbcd6dc2729cc7dda58297a57100c60adcc568
WuX <wux@thenest.dev>
2025-12-10 03:26:30 +0200

    chore: bump flake to 25.11

--- flake.nix
+++ flake.nix
@@ -1,39 +1,43 @@
 {
     inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
         flake-utils.url = "github:numtide/flake-utils";
 
         chariot.url = "github:elysium-os/chariot";
         chariot.inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    outputs = { nixpkgs, flake-utils, ... } @ inputs: flake-utils.lib.eachDefaultSystem (system:
-        let
-            pkgs = import nixpkgs { inherit system; };
-            commonPackages = with pkgs; [
-                inputs.chariot.defaultPackage.${system}
+    outputs =
+        { nixpkgs, flake-utils, ... }@inputs:
+        flake-utils.lib.eachDefaultSystem (
+            system:
+            let
+                pkgs = import nixpkgs { inherit system; };
+                commonPackages = with pkgs; [
+                    inputs.chariot.defaultPackage.${system}
 
-                wget # Required by Chariot
-                libarchive # Required by Chariot
+                    wget # Required by Chariot
+                    libarchive # Required by Chariot
 
-                python3 # Used heavily by tools
+                    python3 # Used heavily by tools
 
-                llvmPackages_20.clang-tools # clang-format & clang-tidy
+                    llvmPackages_20.clang-tools # clang-format & clang-tidy
 
-                gdb
-                bochs
-                qemu_full
-                (pkgs.writeShellScriptBin "qemu-ovmf-x86-64" ''
-                    ${pkgs.qemu_full}/bin/qemu-system-x86_64 \
-                        -drive if=pflash,unit=0,format=raw,file=${pkgs.OVMF.fd}/FV/OVMF.fd,readonly=on \
-                        "$@"
-                '')
-            ];
-        in {
-            devShells.default = pkgs.mkShell {
-                shellHook = "export NIX_SHELL_NAME='elysium-os'";
-                nativeBuildInputs = commonPackages;
-            };
-        }
-    );
+                    gdb
+                    bochs
+                    qemu_full
+                    (pkgs.writeShellScriptBin "qemu-ovmf-x86-64" ''
+                        ${pkgs.qemu_full}/bin/qemu-system-x86_64 \
+                            -drive if=pflash,unit=0,format=raw,file=${pkgs.OVMF.fd}/FV/OVMF.fd,readonly=on \
+                            "$@"
+                    '')
+                ];
+            in
+            {
+                devShells.default = pkgs.mkShell {
+                    shellHook = "export NIX_SHELL_NAME='elysium-os'";
+                    nativeBuildInputs = commonPackages;
+                };
+            }
+        );
 }
