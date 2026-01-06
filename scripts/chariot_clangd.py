50e0db43beeb4d086daeca59b4e7e633296365bf
WuX <wux@thenest.dev>
2026-01-06 21:09:51 +0200

    feat: allow clangd to do header-insertion

--- scripts/chariot_clangd.py
+++ scripts/chariot_clangd.py
@@ -33,6 +33,6 @@ result = subprocess.run(
         "XDG_CACHE_HOME=/root/clangd/cache",
         "-m",
         recipe_source_path + "=" + "/chariot/sources/" + sys.argv[2] + ":ro",
-        f"clangd --background-index --clang-tidy --header-insertion=never --path-mappings {','.join(mappings)}",
+        f"clangd --background-index --clang-tidy --path-mappings {','.join(mappings)}",
     ]
 )
